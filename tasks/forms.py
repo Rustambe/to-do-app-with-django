8432079a2d3874b1744add2d198f0a375e134915
Rustambe <72885188+Rustambe@users.noreply.github.com>
2021-09-15 13:15:27 +0500

    Initial commit

---
+++ tasks/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.forms import ModelForm
+from .models import *
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = "__all__"
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'complete': forms.CheckboxInput(attrs={'class': 'form-check-label'}),
+        }
+
+
+
